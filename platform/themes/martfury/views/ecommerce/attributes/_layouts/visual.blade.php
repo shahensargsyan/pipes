eedf45ecce243946fe4f5c91140d4d3499e7db6b
Shahen <sargsyanshahen@gmail.com>
2021-04-01 18:48:42 +0400

    default color selection issue

--- platform/themes/martfury/views/ecommerce/attributes/_layouts/visual.blade.php
+++ platform/themes/martfury/views/ecommerce/attributes/_layouts/visual.blade.php
@@ -4,8 +4,8 @@
             @foreach($attributes->where('attribute_set_id', $set->id) as $attribute)
                 <li data-slug="{{ $attribute->slug }}" class="attribute-swatch-item"
                     title="{{ $attribute->title }}">
-                    <div class="custom-radio choose_color">
-                        <label class=" purple_color {{ in_array($attribute->id, $selected) ? 'active' : '' }}">
+                    <div class="custom-radio choose_color {{ in_array($attribute->id, $selected) ? 'active_color' : '000' }}">
+                        <label class=" purple_color">
                             <input class="form-control product-filter-item color-check" type="radio" name="attribute_{{ $set->slug }}" value="{{ $attribute->id }}" {{ in_array($attribute->id, $selected) ? 'checked' : '' }}>
                             <label for="purple"><p  style="{{ $attribute->image ? 'background-image: url(' . RvMedia::getImageUrl($attribute->image) . ');' : 'background-color: ' . $attribute->color . ';' }}" class="color"></p></label>
                         </label>
