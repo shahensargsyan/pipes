3fc600af8a7fb7d0320b0e0824ad7a4d81ceb57e
Shahen <sargsyanshahen@gmail.com>
2021-04-13 17:27:28 +0400

    facebook replace code

--- platform/themes/martfury/partials/pipes-header.blade.php
+++ platform/themes/martfury/partials/pipes-header.blade.php
@@ -37,6 +37,7 @@
 
     <!-- Facebook Pixel Code -->
     <script>
+        <!-- Facebook Pixel Code -->
         !function(f,b,e,v,n,t,s)
         {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
             n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -45,11 +46,11 @@
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1334681536912597');
+        fbq('init', '1268049090256331');
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=1334681536912597&ev=PageView&noscript=1"
+                   src="https://www.facebook.com/tr?id=1268049090256331&ev=PageView&noscript=1"
         /></noscript>
     <!-- End Facebook Pixel Code -->
 
