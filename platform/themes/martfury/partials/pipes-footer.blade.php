1e23db37458ddaee2d9dfa15760583729c4a4b33
Shahen <sargsyanshahen@gmail.com>
2021-04-13 17:23:38 +0400

    facebook pixel js code added

--- platform/themes/martfury/partials/pipes-footer.blade.php
+++ platform/themes/martfury/partials/pipes-footer.blade.php
@@ -130,6 +130,30 @@
         token: "677676e1-0a48-4440-8476-e5ae23b28f04",
         host: "https://wchat.freshchat.com"
     });
+
+    <!-- Facebook Pixel Code -->
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '1268049090256331');
+    fbq('track', 'PageView');
+
 </script>
+<noscript><img height="1" width="1" style="display:none"
+               src="https://www.facebook.com/tr?id=1268049090256331&ev=PageView&noscript=1"
+    /></noscript>
+<!-- End Facebook Pixel Code -->
 </body>
 </html>
+
+<script>
+
+</script>
+
+
+
