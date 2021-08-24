9d1ebd9444968b1a0f8dac36768259985cfcbf42
Shahen <sargsyanshahen@gmail.com>
2021-08-24 13:54:01 +0400

    enable chrone for currency update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,6 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Console\Commands\ChargeApprovedPayments;
+use App\Console\Commands\UpdateCurrencyRates;
 
 class Kernel extends ConsoleKernel
 {
@@ -26,6 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('paypal:charge-approved-payments')->everyFiveMinutes();
+        $schedule->command('currencies:update')->daily();
     }
 
     /**
