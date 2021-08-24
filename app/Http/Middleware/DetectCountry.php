206e14cd114908fb3b05a3ab6be11f0ad0c987ad
Shahen <sargsyanshahen@gmail.com>
2021-08-24 12:39:44 +0400

    add detect country middlware

---
+++ app/Http/Middleware/DetectCountry.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Botble\Ecommerce\Repositories\Interfaces\CurrencyInterface;
+use Closure;
+use Illuminate\Http\Request;
+
+class DetectCountry
+{
+    /**
+     * @var CurrencyInterface
+     */
+    protected $currencyRepository;
+
+    /**
+     * PublicEcommerceController constructor.
+     * @param CurrencyInterface $currencyRepository
+     */
+    public function __construct(CurrencyInterface $currencyRepository)
+    {
+        $this->currencyRepository = $currencyRepository;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $geoData = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip=3.115.255.255'));
+
+        if(cms_currency()->getApplicationCurrency()->title != $geoData['geoplugin_currencyCode']) {
+            $currency = $this->currencyRepository->getFirstBy(['title' => $geoData['geoplugin_currencyCode']]);
+            if ($currency){
+                cms_currency()->setApplicationCurrency($geoData['geoplugin_currencyCode']);
+            }
+        };
+
+        return $next($request);
+
+    }
+}
